26003ab295490814b8ee272c8524a63ba5352fc1
Hanna Marks <hanna@wherehab.com>
2018-11-22 14:04:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
+def display_board (board)
   puts"   |   |   "
   puts"-----------"
   puts"   |   |   "
   puts"-----------"
   puts"   |   |   "
 end
+
  puts "Welcome to Tic Tac Toe!"
 
-board = ["hello"," "," "]
